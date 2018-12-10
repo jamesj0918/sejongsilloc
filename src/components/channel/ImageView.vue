f4d4cb78b016c4b109c08bbeb85c52bcce5d7b91
leewoojin <mingwoojin@naver.com>
2018-12-10 20:26:06 +0900

    imageview in article

--- src/components/channel/ImageView.vue
+++ src/components/channel/ImageView.vue
@@ -1,14 +1,11 @@
 <template>
-    <div>
-        <div>
-            <div id="imageWrap">
-                <div id = "image" v-for="image in image_data ">
-                    <img :src="url+image.image"/>
-                </div>
+    <div id="ImageView">
+        <div id="imageWrap">
+            <div class = "image" v-for="image in image_data ">
+                <img :src="url+image.image"/>
             </div>
+            <div id="moreBtn"><div id="moreBtnContent">+ 더보기</div></div>
         </div>
-
-
     </div>
 </template>
 
@@ -29,23 +26,43 @@
         margin: 0;
         padding: 0;
     }
-    img{
-        width: 80%;
-        margin: auto;
-    }
-    #imageWrap{
-        display: inline-block;
-        position: relative;
-        height: 15vh;
+
+    #ImageView {
         width: 100%;
-        border: 1px solid;
-    }
-    #image{
-        display: block;
-        width: 20%;
-        height: 15vh;
-        border: 1px solid;
     }
 
+    #imageWrap {
+        width: 100%;
+    }
 
+    .image {
+        width: 8vw; height: 8vw;
+        margin-right: 3px;
+        border: solid 1px lightgray;
+        line-height: 8vw;
+        display: inline-block;
+        vertical-align: middle;
+        text-align: center;
+    }
+
+    img {
+        width: 80%;
+        vertical-align: middle;
+    }
+
+    #moreBtn {
+        width: 8vw; height: 8vw;
+        line-height: 8vw;
+        text-align: center;
+        color: #8c151f;
+        vertical-align: middle;
+        display: inline-block;
+
+    }
+
+    #moreBtnContent {
+        vertical-align: middle;
+        font-size: 13px;
+        font-weight: bold;
+    }
 </style>
